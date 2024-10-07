1e12d315f811c758513c2c26f67bfa4333c79271
Tasmia57 <tasmiatish57@gmail.com>
2024-10-08 01:46:19 +0600

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,6 @@
-num=(input("Enter Some Digits: "))
-count=len((num))
-print("Total Digits ",num,"are",count)
+def count_digits(s):
+    return len([char for char in s if char.isdigit()])
+
+s=input("Enter a number: ")
+result=count_digits(s)
+print("Number of digits in", s, "is", result)
